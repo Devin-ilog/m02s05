31456b95803043d68ee944b656397ad60e01cb1f
Luis Carlos Elias <luiselias@gmail.com>
2023-02-15 14:50:47 -0300

    Ajustes

---
+++ codigos-aula/modulos/divisao-de-codigo.js
@@ -0,0 +1,33 @@
+//index.html
+
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width">
+  <title>exrcícios de módulo</title>
+  <link href="style.css" rel="stylesheet" type="text/css" />
+</head>
+
+<body>
+
+
+  <script src="constantes.js"></script>
+  <script src="teste.js"></script>
+  <script src="script.js"></script>
+</body>
+
+</html>
+
+//script.js
+
+console.log(nome);
+bacaninha();
+
+//constantes.js
+const nome = 'Luis Carlos';
+
+function bacaninha() {
+  console.log('Escreve algo');
+}
